828d8a7b613430b10af3b421d08400cf578640d7
Nishida Ryousuke <55966582+24daryo@users.noreply.github.com>
2020-12-19 16:57:16 +0900

    update

--- FIle/save.php
+++ FIle/save.php
@@ -7,7 +7,6 @@
 //各端末から送られてくるデータを読み込む
 //$params = filter_input(INPUT_POST, 'faceAttributes',FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);//配列
 
-
 $ids=$_POST['ary'];
 $data = json_encode($_POST['ary']);
 $dataArray = json_decode($data,true);
